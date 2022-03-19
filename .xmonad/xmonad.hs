16dc00e388e724b8d86c7a3c11505c41759a48cd
Aly Anser <staiz7071@gmail.com>
2022-03-19 13:16:41 +0500

    upd xmonad

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -10,6 +10,11 @@ import XMonad.Util.WorkspaceCompare
 
 import XMonad.Hooks.EwmhDesktops
 import XMonad.Hooks.ManageHelpers
+import XMonad.Hooks.ManageDocks
+import XMonad.Hooks.ShowWName
+import XMonad.Hooks.StatusBar
+import XMonad.Hooks.StatusBar.PP
+import XMonad.Hooks.DynamicLog
 
 import XMonad.Layout.Maximize
 import XMonad.Layout.Spacing
@@ -19,6 +24,8 @@ import XMonad.Layout.Accordion
 import XMonad.Layout.Fullscreen
 import XMonad.Layout.Simplest
 import XMonad.Layout.MultiToggle
+import XMonad.Layout.Renamed
+import XMonad.Layout.ThreeColumns
 
 import XMonad.Actions.Search
 import XMonad.Actions.Promote
@@ -29,78 +36,147 @@ import qualified XMonad.StackSet as W
 
 ------------------------------------------------------------------
 
-main::IO()
-main = xmonad . ewmh $ def {
+main = xmonad . ewmhFullscreen . ewmh. xmobarProp $ def {
 	terminal = my_terminal,
-         modMask = my_mod_mask,
+	modMask = my_mod_mask,
 	borderWidth = my_border_width,
 	normalBorderColor = my_normal_border_color, 
 	focusedBorderColor = my_focused_border_color,
 	layoutHook = my_layout_hook,
 	focusFollowsMouse = my_focus_follows_mouse,
 	manageHook = namedScratchpadManageHook scratchpads,
-	clickJustFocuses = my_click_just_focuses }
+	logHook = showWNameLogHook my_swn_config,
+	clickJustFocuses = my_click_just_focuses
+}
 	`additionalKeysP` my_keys
 
 my_terminal = "kitty -1"
 my_normal_border_color = "#3b4252"
 my_focused_border_color = "#bc96da"
-my_layout_hook = my_layout
 my_focus_follows_mouse = False
 my_click_just_focuses = False
 my_border_width = 0
 my_mod_mask = mod4Mask
+my_spacing = 7
+my_font = "xft:sf pro rounded:size=9:antialias=true:hinting=true"
+my_status_bar = statusBarProp "xmobar" (pure my_pp)
+my_layout_hook = tall ||| accordion ||| mirror_accordion ||| tab ||| full
 
-my_layout = maximizeWithPadding 0 
-         $ fullscreenFloat
-         $ hiddenWindows 
-         $ tall ||| accordion ||| tab ||| full
-	where
-		tall = spacing 10 $ Tall nmaster delta ratio
-		accordion = spacing 10 $ Accordion
-		full = Full
-		tab = tabbed shrinkText my_tab_config
-		nmaster = 1
-		ratio = 1 / 2
-		delta = 2 / 100
+background_color = "#282a36"
+current_line_color = "#44475a"
+selection_color = "#44475a"
+foreground_color = "#f8f8f2"
+comment_color = "#6272a4"
+cyan_color = "#8be9fd"
+green_color = "#50fa7b"
+orange_color = "#ffb86c"
+pinkColor = "#ff79c6"
+purple_color = "#bd93f9"
+red_color = "#ff5555"
+yellowColor = "#f1fa8c"
 
-my_xp_config = def {
-	height = 50,
-	promptBorderWidth = 0
+my_tab_theme = def { 
+	fontName = my_font,
+	activeColor = my_focused_border_color,
+	inactiveColor = my_normal_border_color,
+	activeBorderColor = my_focused_border_color,
+	inactiveBorderColor = my_normal_border_color,
+	activeTextColor = my_normal_border_color,
+	inactiveTextColor = purple_color
 }
 
-scratchpads = [ NS "terminal" spawn_term find_term manage_term
-                , NS "docs" spawn_docs find_docs manage_docs
-                ]
-         where
-                  spawn_term  = "kitty -1 --class scratch_terminal"
-                  find_term   = className =? "scratch_terminal"
-                  manage_term = customFloating $ W.RationalRect l t w h
-                           where
-                                    h = 0.90 -- terminal height
-                                    w = 0.95 -- terminal width
-                                    t = 0.05 -- distance from top edge
-                                    l = 0.025 -- distance from left edge
-                  spawn_docs  = "~/.local/bin/devdocs"
-                  find_docs   = className =? "FFPWA-01FX9RNFXCWG4QS358C257Y11S"
-                  manage_docs = customFloating $ W.RationalRect l t w h
-                           where
-                                    h = 0.95 -- terminal height
-                                    w = 0.95 -- terminal width
-                                    t = 0.025 -- distance from top edge
-                                    l = 0.025 -- distance from left edge
+my_pp = def {
+	ppTitle = xmobarColor orange_color "" . shorten 25,
+	ppLayout = xmobarColor purple_color "",
+	ppCurrent = xmobarColor green_color "" . wrap "(" ")",
+	ppUrgent = xmobarColor red_color "" . wrap "[" "]",
+	ppHidden = xmobarColor foreground_color "",
+	ppVisible = xmobarColor orange_color "" . wrap "(" ")",
+	ppSep = xmobarColor cyan_color "" "}—————{",
+	ppWsSep = xmobarColor cyan_color "" "}—{",
+	ppOrder = \(ws:l:t:ex)  -> [ws]++ex++[t,l] -- {workspaces}-{title}--{layout}
+}
 
-my_tab_config = def { activeColor = "#556064"
-                  , inactiveColor = "#2F3D44"
-                  , urgentColor = "#FDF6E3"
-                  , activeBorderColor = "#454948"
-                  , inactiveBorderColor = "#454948"
-                  , urgentBorderColor = "#268BD2"
-                  , activeTextColor = "#80FFF9"
-                  , inactiveTextColor = "#1ABC9C"
-                  , urgentTextColor = "#1ABC9C"
-                  , fontName = "xft:ubuntu:size=0"
-                  }
+mirror_accordion = renamed [Replace "mirror accor"] 
+	$ maximizeWithPadding 0
+	$ avoidStruts
+	$ hiddenWindows 
+	$ spacing my_spacing 
+	$ Mirror Accordion 
+
+tall = renamed [Replace "tall"] 
+	$ maximizeWithPadding 0
+	$ avoidStruts
+	$ hiddenWindows 
+	$ spacing my_spacing 
+	$ addTabs shrinkText my_tab_theme 
+	$ Tall nmaster delta ratio
+
+accordion = renamed [Replace "accor"] 
+	$ maximizeWithPadding 0
+	$ avoidStruts
+	$ hiddenWindows 
+	$ spacing my_spacing 
+	$ Accordion
+
+full = renamed [Replace "full"] 
+	$ maximizeWithPadding 0
+	$ avoidStruts
+	$ hiddenWindows 
+	$ Full
+
+tab = renamed [Replace "tabbed"] 
+	$ maximizeWithPadding 0
+	$ avoidStruts
+	$ hiddenWindows
+	$ tabbed shrinkText my_tab_theme
+	 
+nmaster = 1
+ratio = 1 / 2
+delta = 2 / 100
+
+my_swn_config = def { 
+	swn_font = "xft:Ubuntu:bold:size=60",
+	swn_fade = 0.5,
+	swn_bgcolor = "#1c1f24",
+	swn_color = "#ffffff"
+}
+
+scratchpads = [
+	NS "terminal" spawn_term find_term manage_term,
+	NS "docs" spawn_docs find_docs manage_docs
+	]
+	where
+		spawn_term = "kitty -1 --class scratch_terminal"
+		find_term = className =? "scratch_terminal"
+		manage_term = customFloating $ W.RationalRect l t w h
+			where
+				h = 0.95 -- height
+				w = 0.95 -- width
+				t = 0.033 -- distance from top edge
+				l = 0.025-- distance from left edge
+
+		spawn_docs = "~/.local/bin/devdocs"
+		find_docs = className =? "FFPWA-01FX9RNFXCWG4QS358C257Y11S"
+		manage_docs = customFloating $ W.RationalRect l t w h
+			where
+				h = 0.95 -- height
+				w = 0.95 -- width
+				t = 0.025 -- distance from top edge
+				l = 0.025 -- distance from left edge
+
+my_tab_config = def {
+	activeColor = "#556064",
+	inactiveColor = "#2F3D44",
+	urgentColor = "#FDF6E3",
+	activeBorderColor = "#454948",
+	inactiveBorderColor = "#454948",
+	urgentBorderColor = "#268BD2",
+	activeTextColor = "#80FFF9",
+	inactiveTextColor = "#1ABC9C",
+	urgentTextColor = "#1ABC9C",
+	fontName = "xft:ubuntu:size=0"
+}
 
 my_keys = [
 		("<XF86AudioLowerVolume>", spawn "pulseaudio-ctl down"),
@@ -118,28 +194,20 @@ my_keys = [
 		("C-q", namedScratchpadAction scratchpads "terminal"),
 		("M-e", namedScratchpadAction scratchpads "docs"),
 		("M-g", selectSearch google),
-		("M-l", nextNonEmptyWS),
-                  ("M-h", prevNonEmptyWS),
+		("M-l", next_non_empty_ws),
+		("M-h", prev_non_empty_ws),
 		("M-\\", toggleWS' ["NSP"]),
-		("M-c", moveTo Next EmptyWS),
+		("M-c", moveTo Next emptyWS),
+		("M-[", sendMessage Shrink),
+		("M-]", sendMessage Expand),
+		("M-b", withFocused $ windows . W.sink),
 		("M-S-r", spawn "xmonad --recompile && xmonad --restart")
 	]
-		
--- any workspace but scratchpad
-notSP = (return $ ("NSP" /=) . W.tag) :: X (WindowSpace -> Bool)
-shiftAndView dir = findWorkspace getSortByIndex dir (WSIs notSP) 1
-        >>= \t -> (windows . W.shift $ t) >> (windows . W.greedyView $ t)
 
--- hidden, non-empty workspaces less scratchpad
-shiftAndView' dir = findWorkspace getSortByIndexNoSP dir HiddenNonEmptyWS 1
-        >>= \t -> (windows . W.shift $ t) >> (windows . W.greedyView $ t)
-nextNonEmptyWS = findWorkspace getSortByIndexNoSP Next HiddenNonEmptyWS 1
-        >>= \t -> (windows . W.view $ t)
-prevNonEmptyWS = findWorkspace getSortByIndexNoSP Prev HiddenNonEmptyWS 1
-        >>= \t -> (windows . W.view $ t)
-getSortByIndexNoSP =
-        fmap (.namedScratchpadFilterOutWorkspace) getSortByIndex
-
--- toggle any workspace but scratchpad
-myToggle = windows $ W.view =<< W.tag . head . filter 
-        ((\x -> x /= "NSP" && x /= "SP") . W.tag) . W.hidden
+-- yoinked from somewhere
+not_sp = (return $ ("NSP" /=) . W.tag) :: X (WindowSpace -> Bool)
+shift_and_view dir = findWorkspace getSortByIndex dir (WSIs not_sp) 1 >>= \t -> (windows . W.shift $ t) >> (windows . W.greedyView $ t)
+shift_and_view' dir = findWorkspace get_sort_by_index_no_sp dir HiddenNonEmptyWS 1 >>= \t -> (windows . W.shift $ t) >> (windows . W.greedyView $ t)
+next_non_empty_ws = findWorkspace get_sort_by_index_no_sp Next HiddenNonEmptyWS 1 >>= \t -> (windows . W.view $ t)
+prev_non_empty_ws = findWorkspace get_sort_by_index_no_sp Prev HiddenNonEmptyWS 1 >>= \t -> (windows . W.view $ t)
+get_sort_by_index_no_sp = fmap (.namedScratchpadFilterOutWorkspace) getSortByIndex
