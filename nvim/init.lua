7eb5d481f457c86574c27c7f6311d13b19c05c5d
Ali Anser <alyystaz@gmail.com>
2022-05-30 04:54:41 +0500

    nvim upd

--- nvim/init.lua
+++ nvim/init.lua
@@ -7,6 +7,10 @@ require("packer").startup(function()
 	}
 
 	use {
+		'petertriho/nvim-scrollbar'
+	}
+
+	use {
 		'lewis6991/impatient.nvim', 
 	}
 
@@ -16,6 +20,11 @@ require("packer").startup(function()
 
 	use {
 		'phaazon/hop.nvim',
+	}
+
+	use {
+		'lewis6991/gitsigns.nvim',
+		tag = 'release'
 	}
 
 	use {
@@ -59,19 +68,131 @@ require("packer").startup(function()
 	use {
 		'folke/tokyonight.nvim'
 	}
+
+	use {
+		"nvim-neo-tree/neo-tree.nvim",
+		branch = "v2.x",
+		requires = { 
+			"nvim-lua/plenary.nvim",
+			"kyazdani42/nvim-web-devicons",
+			"MunifTanjim/nui.nvim",
+		}
+	}
+
+	use {
+		'lukas-reineke/indent-blankline.nvim'
+	}
 end)
 
-require("impatient")
+require('impatient')
 require('Comment').setup{}
 require('lualine').setup{}
-require("trouble").setup{}
+require('trouble').setup{}
 require('bufferline').setup{}
+require('gitsigns').setup{}
+
+vim.opt.list = true
+vim.opt.listchars:append("space:⋅")
+-- vim.opt.listchars:append("eol:↴")
+
+require('indent_blankline').setup {
+	show_end_of_line = true,
+	space_char_blankline = " ",
+	show_current_context = false,
+}
+
+local scrollbar_colors = require("tokyonight.colors").setup()
+
+require('scrollbar').setup {
+
+	handle = {
+		color = scrollbar_colors.bg_highlight,
+		hide_if_all_visible = false
+	},
+
+	marks = {
+		Search = { color = scrollbar_colors.orange },
+		Error = { color = scrollbar_colors.error },
+		Warn = { color = scrollbar_colors.warning },
+		Info = { color = scrollbar_colors.info },
+		Hint = { color = scrollbar_colors.hint },
+		Misc = { color = scrollbar_colors.purple },
+	}
+}
 
 require('nvim-treesitter.configs').setup{
 	ensure_installed = { 'cpp', 'c', 'lua', 'haskell', 'python', 'rust', 'cmake', 'bash', 'yaml' },
 
 	highlight = {
 		enable = true
+	},
+}
+
+require("neo-tree").setup {
+	close_if_last_window = true,
+	popup_border_style = "rounded",
+	enable_diagnostics = false,
+	default_component_configs = {
+		indent = {
+			padding = 0,
+			with_expanders = false,
+		},
+		icon = {
+			folder_closed = "",
+			folder_open = "",
+			folder_empty = "",
+			default = "",
+		},
+		git_status = {
+			symbols = {
+				added = "",
+				deleted = "",
+				modified = "",
+				renamed = "➜",
+				untracked = "★",
+				ignored = "◌",
+				unstaged = "✗",
+				staged = "✓",
+				conflict = "",
+			},
+		},
+	},
+	window = {
+		width = 25,
+		mappings = {
+			["o"] = "open",
+		},
+	},
+	filesystem = {
+		filtered_items = {
+			visible = false,
+			hide_dotfiles = true,
+			hide_gitignored = false,
+			hide_by_name = {
+				".DS_Store",
+				"thumbs.db",
+				"node_modules",
+				"__pycache__",
+			},
+		},
+		follow_current_file = true,
+		hijack_netrw_behavior = "open_current",
+		use_libuv_file_watcher = true,
+	},
+	git_status = {
+		window = {
+			position = "float",
+		},
+	},
+	event_handlers = {
+		{
+			event = "vim_buffer_enter",
+			handler = function(_)
+				if vim.bo.filetype == "neo-tree" then
+					vim.wo.signcolumn = "auto"
+				end
+		end,
+		},
 	},
 }
 
@@ -82,11 +203,11 @@ lsp["clangd"].setup{
 	command = { 'clangd', '--background-index', '-j=4', '--head-insertion=never', '--clang-tidy=false', '--completion-style=detailed' },
 }
 
-local rest_lang_servers = { "cmake", "bashls", "pyright" }
-
-for _, name in ipairs(rest_lang_servers) do
-	lsp[name].setup{}
-end
+-- local rest_lang_servers = { "cmake", "bashls", "pyright" }
+--
+-- for _, name in ipairs(rest_lang_servers) do
+--	lsp[name].setup{}
+-- end
 
 -- general
 
@@ -98,7 +219,9 @@ vim.g.softtabstop = tab_len
 vim.g.noexpandtab = true
 
 vim.opt.number = true
+vim.opt.relativenumber = true
 vim.opt.guicursor = nil
+vim.opt.termguicolors = true
 
 vim.g.nofixedenofline = true
 vim.g.loadedmatchparen = false
